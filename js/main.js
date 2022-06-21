54d21945d9002188cdf6994beca6f2d861e50754
AmrDev95 <amr_sayed95@icloud.com>
2022-06-21 16:04:20 +0200

    Added main Project

---
+++ js/main.js
@@ -0,0 +1,67 @@
+let toggler =0;
+let notificationToggler =0;
+
+$ ("#addAction").mouseenter(function(){
+    $("#addActionImg").attr("src", "images/star-dynamic-premium.png");
+});
+
+$ ("#addAction").mouseleave(function(){
+    $("#addActionImg").attr("src", "images/star-dynamic-clay.png");
+});
+
+$ ("#statistics").mouseenter(function(){
+    $("#statisticsImg").attr("src", "images/chart-dynamic-premium.png");
+});
+
+$ ("#statistics").mouseleave(function(){
+    $("#statisticsImg").attr("src", "images/chart-dynamic-clay.png");
+});
+
+$ ("#settings").mouseenter(function(){
+    $("#settingsImg").attr("src", "images/setting-dynamic-premium.png");
+});
+
+$ ("#settings").mouseleave(function(){
+    $("#settingsImg").attr("src", "images/setting-dynamic-clay.png");
+});
+
+$ ("#chat").mouseenter(function(){
+    $("#chatImg").attr("src", "images/chat-text-dynamic-premium.png");
+});
+
+$ ("#chat").mouseleave(function(){
+    $("#chatImg").attr("src", "images/chat-text-dynamic-clay.png");
+});
+
+$ ( "#status" ).click(function(){
+    if(toggler==0){
+        $( "#activeStatus" ).html("Deactivated");
+        $("#statusImg").attr("src",'images/bell-dynamic-color.png' );
+        $(".activeHover").css("background-color", "rgb(203,68,74)");
+        $(".activehover").css("border", "2px solid rgb(203,68,74)");
+        toggler = 1;
+    }
+
+    else{
+        $( "#activeStatus" ).html("Active");
+        $("#statusImg").attr("src",'images/bell-dynamic-premium.png' );
+        $(".activeHover").css("background-color", "var(--main-color)");
+        $(".activehover").css("border", "2px solid var(--main-color)");
+        toggler = 0;
+    }
+});
+
+$( "#notificationButton" ).click(function(){
+    if(notificationToggler==0){
+        $("#notificationDiv").animate({transform: "translate(-100%)"}, 1000);
+        notificationToggler=1;
+        console.log("hello");
+    }
+
+    else{
+        $("#notificationDiv").animate({transform : "100%"}, 1000);
+        notificationToggler=0;
+        console.log("aaaa");
+    }
+})
+
